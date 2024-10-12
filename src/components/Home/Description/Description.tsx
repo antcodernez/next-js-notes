640c30f741c8bdade5ad09b8fc87226ea2a87c85
Antonio Jiménez <marshday5@gmail.com>
2024-10-11 23:09:52 -0600

    feat(public): implementing public files

--- src/components/Home/Description/Description.tsx
+++ src/components/Home/Description/Description.tsx
@@ -2,6 +2,7 @@ export const Description = () => {
     return (
         <section>
             <h1>Description about products</h1>
+            <img src="/images/angry-cat.png" alt="Angry cat" />
         </section>
     )
 }
