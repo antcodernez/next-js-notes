77ec1bf0f27e73ff8f5ac4587142c9f4dcac0f04
Antonio Jiménez <marshday5@gmail.com>
2024-10-15 23:30:30 -0600

    feat(next-image): implementing the next image component for a better performance

--- src/components/Home/Description/Description.tsx
+++ src/components/Home/Description/Description.tsx
@@ -1,8 +1,18 @@
+import Image from "next/image"
+import styles from "./Description.module.sass"
+
 export const Description = () => {
     return (
-        <section>
+        <section className={styles.Description}>
             <h1>Description about products</h1>
-            <img src="/images/angry-cat.png" alt="Angry cat" />
+            <Image 
+                src="/images/angry-cat.png" 
+                alt="Angry cat" 
+                width={500} 
+                height={300} 
+                priority={false} 
+                quality={7}
+            />
         </section>
     )
 }
